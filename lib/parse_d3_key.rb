0031b0192a7d73c4920225d4c44b59868e45a814
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-04-06 20:00:55 -0400

    parse_d3_key: check for nil blizzard_screen_name and tweet_screen_name

--- lib/parse_d3_key.rb
+++ lib/parse_d3_key.rb
@@ -6,16 +6,15 @@
 #
 #  @return key or nil
 def get_d3_key blizzard_screen_name, tweet_screen_name, tweet_text
-  return nil unless blizzard_screen_name == tweet_screen_name
+  return nil unless blizzard_screen_name and tweet_screen_name and blizzard_screen_name == tweet_screen_name
   parse_d3_key tweet_text
 end
 
 
+# diablo 3 beta key format: 6-4-6-4-6 alphanumeric characters (length 26)
 KEY_PATTERN = /(?<key>[a-zA-Z0-9]{6}-*[a-zA-Z0-9]{4}-*[a-zA-Z0-9]{6}-*[a-zA-Z0-9]{4}-*[a-zA-Z0-9]{6})/
+# TODO allow whitespace or dashes between char groups
 
-# TODO - test match nil
-
-# the only assumption about the tweeted key is it's of length 20, with whatever separating it
 
 # private. return nil on parse failure xor d3_key
 def parse_d3_key text
