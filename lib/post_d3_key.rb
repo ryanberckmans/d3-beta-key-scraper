57366ac297582aa907a6c97997aec02948b9aabd
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-04-08 02:04:50 -0400

    post_d3_key rm tmp/* before curl to clear cookies

--- lib/post_d3_key.rb
+++ lib/post_d3_key.rb
@@ -9,9 +9,10 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko
 #
 #  @return nil
 def post_d3_key account_name, password, d3_key
-  log_file = "log/" + d3_key + Time.now.utc.to_s.gsub(/\s/, ",")
+  log_file = "log/" + d3_key + "-" + Time.now.utc.to_s.gsub(/\s/, "-")
   `mkdir -p log`
   `mkdir -p tmp`
+  `rm tmp/*`
   
   `curl --user-agent "#{USER_AGENT}" -v -L --cookie-jar tmp/cookie -d "accountName=#{account_name}&password=#{password}&persistLogin=on" https://us.battle.net/login/en/ 2>&1 >> #{log_file}`
   `curl --user-agent "#{USER_AGENT}" -v --cookie tmp/cookie --cookie-jar tmp/cookie2 -d "gameKey=#{d3_key}" https://us.battle.net/account/management/add-game.html  2>&1 >> #{log_file}`
