73c8c0252b8f364f01a594d47bebe7bbe6971b61
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-04-09 18:10:56 -0400

    push post_d3_key inside eventmachine loop

--- lib/get_d3_key_from_twitter.rb
+++ lib/get_d3_key_from_twitter.rb
@@ -1,5 +1,6 @@
 require "twitter_stream"
 require "parse_d3_key"
+require "post_d3_key"
 
 class D3KeyFromTwitter
 
@@ -7,19 +8,25 @@ class D3KeyFromTwitter
   #
   #  @param twitter_login - credentials to access twitter stream "USER:PASS"
   #  @param stream_filter - querystring to append to twitter's filter.json?
-  #  @param blizzard_screen_name
-  def get_d3_key_from_twitter twitter_login, stream_filter, blizzard_screen_name
+  #  @param blizzard_screen_name - tweeter of diablo 3 beta key
+  #  @param battlenet_user - battle.net account to post key to
+  #  @param battlenet_password - password of battle.net account to post key to
+  #
+  #  @return nil
+  def activate_d3_key_from_twitter twitter_login, stream_filter, blizzard_screen_name, battlenet_user, battlenet_password
     d3_key = nil
     twitter_stream twitter_login, stream_filter do |screen_name, tweet_text|
       puts "#{screen_name}: #{tweet_text}\n"
       d3_key = parse_d3_key blizzard_screen_name, screen_name, tweet_text
       if d3_key
         puts "found d3 key: #{d3_key}"
+        d3_key_stripped = d3_key.gsub /[^a-zA-Z0-9]/, ''
+        post_d3_key battlenet_user, battlenet_password, d3_key_stripped
         true # true ends twitter stream
       else
         false # false continues twitter stream
       end
     end
-    d3_key.gsub /[^a-zA-Z0-9]/, ''
+    nil
   end
 end
--- spec/get_d3_key_from_twitter_spec.rb
+++ spec/get_d3_key_from_twitter_spec.rb
@@ -1,10 +1,12 @@
-require "get_d3_key_from_twitter"
+require "activate_d3_key_from_twitter"
 
-describe "get_d3_key_from_twitter" do
-  it "calls twitter_stream, forwards results to parse_d3_key, and returns successful key" do
+describe "activate_d3_key_from_twitter" do
+  it "calls twitter_stream, forwards results to parse_d3_key, and posts successful key" do
     twitter_login = "foo:bar"
     stream_filter = "some_filter,romg,1334"
     blizzard_screen_name = "jim"
+    battlenet_user = "fred"
+    battlenet_password = "battlent#*pasS123"
     tweet_text = "abc def sdfkljkl"
     key = "the key\n2308023498  \t #*#  "
     key_stripped = "thekey2308023498"
@@ -12,9 +14,11 @@ describe "get_d3_key_from_twitter" do
     D3KeyFromTwitter.any_instance.should_receive(:twitter_stream).with(twitter_login, stream_filter).once.and_yield(blizzard_screen_name,tweet_text).and_yield(blizzard_screen_name,tweet_text).and_yield(blizzard_screen_name,tweet_text).and_return(nil)
 
     D3KeyFromTwitter.any_instance.should_receive(:parse_d3_key).with(blizzard_screen_name, blizzard_screen_name, tweet_text).exactly(3).times.and_return(nil,nil,key)
-    
-    result_key = D3KeyFromTwitter.new.get_d3_key_from_twitter twitter_login, stream_filter, blizzard_screen_name
 
-    result_key.should eq(key_stripped)
+    D3KeyFromTwitter.any_instance.should_receive(:post_d3_key).with(battlenet_user, battlenet_password, key_stripped).once.and_return(nil)
+    
+    result_key = D3KeyFromTwitter.new.activate_d3_key_from_twitter twitter_login, stream_filter, blizzard_screen_name, battlenet_user, battlenet_password
+
+    result_key.should eq(nil)
   end
 end
